45f382b73e8129cae66637d969f98c541a3bd641
Felipe11212 <felipesanchezf816@gmail.com>
2025-06-23 20:43:44 -0500

    Update galeria.blade.php

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -2,18 +2,38 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <!-- Adaptabilidad a dispositivos móviles -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Galería</title>
+
+    <!-- Enlace a estilos personalizados -->
     <link rel="stylesheet" href="{{ asset('css/galeria.css') }}">
+
+    <!-- Iconos de Bootstrap (usado para íconos como flechas, etc.) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Ícono en la pestaña del navegador -->
     <link rel="icon" href="{{ asset('imgs/logo azul.jpg') }}">
 </head>
 <body>
+
+    <!-- Separación visual superior -->
     <br><br>
-    <p class="heading">Galería De <span style="color: rgb(121, 179, 255)">Micro</span><span style="color: rgb(101, 102, 167)">Stock</span></p>
+
+    <!-- Título principal con estilos separados por color -->
+    <p class="heading">
+        Galería De 
+        <span style="color: rgb(121, 179, 255)">Micro</span>
+        <span style="color: rgb(101, 102, 167)">Stock</span>
+    </p>
+
+    <!-- Más espacio entre título y galería -->
     <br><br><br>
 
+    <!-- Primera fila de la galería -->
     <div class="gallery-image">
+        <!-- Imagen 1 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im2.jpg') }}" alt="Inicios" />
             <div class="transparent-box">
@@ -24,6 +44,7 @@
             </div> 
         </div>
 
+        <!-- Imagen 2 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im1.jpg') }}" alt="Sistema" />
             <div class="transparent-box">
@@ -34,6 +55,7 @@
             </div>
         </div>
 
+        <!-- Imagen 3 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im3.jpg') }}" alt="Estructura" />
             <div class="transparent-box">
@@ -45,7 +67,9 @@
         </div>
     </div>
 
+    <!-- Segunda fila de la galería -->
     <div class="gallery-image">
+        <!-- Imagen 4 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im4.jpg') }}" alt="Proyección" />
             <div class="transparent-box">
@@ -56,6 +80,7 @@
             </div> 
         </div>
 
+        <!-- Imagen 5 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im5.jpg') }}" alt="Imaginación" />
             <div class="transparent-box">
@@ -66,6 +91,7 @@
             </div> 
         </div>
 
+        <!-- Imagen 6 -->
         <div class="img-box">
             <img src="{{ asset('imgs/im6.jpg') }}" alt="Estadísticas" />
             <div class="transparent-box">
@@ -77,11 +103,16 @@
         </div>
     </div>
 
+    <!-- Botón de regreso a la página principal -->
     <a href="{{ route('inicio') }}" class="btn-circle">Volver A Inicio</a>
+
+    <!-- Separación antes del pie de página -->
     <br><br><br><br><br>
 
+    <!-- Pie de página -->
     <div class="copyright text-center">
         <p>© 2025 MicroStock. Todos los derechos reservados.</p>
     </div>
 </body>
 </html>
+
